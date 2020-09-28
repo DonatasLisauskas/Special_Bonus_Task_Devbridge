a6cf905c9da3e931f16024e07f483b339e97d3fd
donli <donliskas@gmail.com>
2020-09-28 15:25:38 +0300

    A call of "Years.print Bonus DatesBetween" method is added on the Main.java file.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            Years.printBonusDatesBetween(1980,2020);
+        } catch (InvalidYearsException ex) {
+            ex.printStackTrace();
+        }
     }
 }
